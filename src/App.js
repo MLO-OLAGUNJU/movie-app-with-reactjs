ef24bf85474a36a6c1e724152276f4d5144b9c5e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-18 15:54:15 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=247f9422";
 
+const movie1 = {
+  Title: "Spiderman",
+  Year: "2010",
+  imdbID: "tt1785572",
+  Type: "movie",
+  Poster: "N/A",
+};
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    console.log(data.Search);
   };
 
   useEffect(() => {
@@ -27,6 +35,31 @@ const App = () => {
           value={"Superman"}
           onChange={() => {}}
         />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
