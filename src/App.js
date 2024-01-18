d58ce2f06f4dc21e1215aa034ecfb0987a4446d2
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-18 15:31:05 +0100

    Updated the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+import "./App.css";
+import SearchIcon from "./search.svg";
+//247f9422
+
+const API_URL = "http://www.omdbapi.com/?apikey=247f9422";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return (
+    <div className="app">
+      <h1>MLO's MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={"Superman"}
+          onChange={() => {}}
+        />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
